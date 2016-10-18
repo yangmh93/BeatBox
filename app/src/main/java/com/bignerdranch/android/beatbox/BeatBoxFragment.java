bdf49d9c62d2725f428bddaa1fce05b0252af3f1
Michael Yang <yangmh@appstate.edu>
2016-10-18 10:48:57 -0400

    plays sound

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -40,18 +40,26 @@ public class BeatBoxFragment extends Fragment {
         return view;
     }
 
-    private class SoundHolder extends RecyclerView.ViewHolder {
+    private class SoundHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
         private Button mButton;
         private Sound mSound;
 
         public SoundHolder(LayoutInflater inflater, ViewGroup container) {
             super(inflater.inflate(R.layout.list_item_sound, container, false));
+
             mButton = (Button)itemView.findViewById(R.id.list_item_sound_button);
+            mButton.setOnClickListener(this);
         }
 
         public void bindSound(Sound sound) {
             mSound = sound;
             mButton.setText(mSound.getName());
+        }
+
+        @Override
+        public void onClick(View v) {
+            mBeatBox.play(mSound);
         }
     }
 
